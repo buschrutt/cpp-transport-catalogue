db984140cc1e760e4e4a12287273bb589b0a47d9
buschrutt <buschrutt@gmail.com>
2022-08-18 18:33:27 -0500

    cpp sprint10: final, domain data added

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -8,8 +8,8 @@ using namespace std::literals;
 int main() {
 
     catalogue::TransportCatalogue catalogue;
-    json_lib::Document json_doc = json_lib::JsonFileLoad("json_source.json"s);
-    //json_lib::Document json_doc = json_lib::JsonConsoleLoad(std::cin);
+    //json_lib::Document json_doc = json_lib::JsonFileLoad("json_source.json"s);
+    json_lib::Document json_doc = json_lib::JsonConsoleLoad(std::cin);
     json_reader::JSONReader json_data(json_doc, catalogue);
     json_data.DBBuilder();
     json_lib::Document json_result = json_data.JsonResponseBuilder();
